87d442e6ea7c3670ef203eada0da64b8e45c0b2b
DaBingXiaoQian <1962129667@qq.com>
2016-10-07 10:12:21 +0800

    10140340148
    王冰倩的点名系统

---
+++ dianming/web/main.dart
@@ -0,0 +1,62 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+import "dart:math";
+var names = new List<int>();
+
+void main() {
+  querySelector('#button')
+      ..onClick.listen(random);
+}
+void random(MouseEvent event)
+{  var students=
+{
+  '0':'平安',
+  '1':'张三',
+  '2':'李四',
+  '3':'王麻',
+  '4':'赵华',
+  '5':'孙悦',
+  '6':'李丽',
+  '7':'周涛',
+  '8':'陈旭',
+  '9':'朱珠',
+  '10':'陈晨',
+  '11':'张宇',
+  '12':'李伟',
+  '13':'王宁',
+  '14':'兰丹',
+  '15':'李斌',
+  '16':'魏毅',
+  '17':'韦俊',
+  '18':'孙华',
+  '19':'安安',
+  '20':'于雷'
+};
+var remain=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  var random=new Random();//生成一个类,8作为一个种子，如果有8的话每次生成的随机数都是相同，没有8的话就会生成不同的随机数。
+ var num=random.nextInt(21); //利用类里面的函数生成0-20的整数随机数,总共21位同学
+querySelector('#num').text='产生的随机数是：'+num.toString();
+  var a=names.contains(num);
+if(a) {
+  if (names.length < 21) {
+    querySelector('#name').text = '这位同学已经点过，请重新生成随机数进行点名！';
+  }
+  else {
+    querySelector('#name').text = '您已经点过班级中所有同学的姓名，继续点击将重新开始点名！';
+    names.clear();
+  }
+}
+else
+{
+  names.add(num);
+  querySelector('#name').text='被点名的同学：'+students[num.toString()]; //text中的值只能是字符串
+
+}
+for(var x in names)
+{remain.remove(x);
+}
+  querySelector("#remain").text='没有点过名的同学编号：'+remain.toString();//显示没有点过名的同学编号
+
+}
