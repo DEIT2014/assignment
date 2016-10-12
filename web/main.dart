c1846408d25e681452587520e81e2ff685ba1059
panchenyu <panchenyu0808@163.com>
2016-10-12 18:11:20 +0800

    实现不重复点名及循环点名功能
    不重复点名功能仍存在问题

--- web/main.dart
+++ web/main.dart
@@ -3,29 +3,37 @@
 
 import 'dart:html';
 import'dart:math';
-
+var keepname =new List<int>();//要移出来不能放在generate函数当中，不然每次都生成keepname数组，数组中永远无法保存已经抽选过的num函数
+var name= {
+  0 : '温顾顾',
+  1 :'秦泱泱',
+  2 :'沈晋',
+  3 :'秦央',
+  4 :'沈彻',
+  5 :'纪澄',
+  6 :'荣旋',
+  7 :'潘晨煜',
+  8 :'刘雯君',
+  9:'王源',
+};
 void main() {
   //querySelector('#output').text = '随机点名程序';
   querySelector('#create_number').onClick.listen(generate);
 }
 void generate(MouseEvent e){
-  //生成random数
-  var randomVar=new Random( );
-  var num=randomVar.nextInt(10);
-  //把数字显示出来
-  var name= {
-    1 : '温顾顾',
-    2 :'秦泱泱',
-    3 :'沈晋',
-    4 :'秦央',
-    5 :'沈彻',
-    6 :'纪澄',
-    7 :'荣旋',
-    8 :'潘晨煜',
-    9 :'刘雯君',
-    10:'王源',
-  };
-  querySelector('#showme').text=name[num];
-  //num.toString();//text里只能接受字符串
-  name[num] == null;
+  var randomVar=new Random( ); //引用一个random的类
+  var num=randomVar.nextInt(10);  //从10的范围中生成一个随机数,从0开始
+  if(keepname.length>10){
+    querySelector('#showme').text='本轮点名已结束，点击按钮进入新一轮点名';
+    keepname.clear();
+  }
+  else{
+    if(keepname.indexOf(num) == 0){//此语句判断num是否在数组中
+      querySelector('#showme').text='此学生本轮已点过，请再次操作';
+    }
+    else{
+      keepname.add(num);
+      querySelector('#showme').text=name[num];
+    }
+  }
 }
