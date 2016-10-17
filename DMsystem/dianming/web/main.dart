7e39fa49a4dae95b2614a1b08028e7b6fccfa309
zhangtiantian1 <15221638601@163.com>
2016-10-17 16:16:48 +0800

    完善点名
    姓名点名

--- DMsystem/dianming/web/main.dart
+++ DMsystem/dianming/web/main.dart
@@ -7,12 +7,31 @@ import 'dart:core';
 
  var count=0; final n=20;
 List<int> list = new List(n); //定义数组
-
+List listname=[['10140340101','王巍'],
+['10140340104','龚馨懿'],
+['100140340105','涂斌琴'],
+['10140340109','杜谦'],
+['10140340110','张红燕'],
+['10140340116','何欣语'],
+['10140340117','陈婧'],
+['10140340119','高佳平'],
+['10140340120','姜慧敏'],
+['10140340122','吴怡雯'],
+['10140340125','池吉'],
+['10140340129','石睿'],
+['10140340131','王海佳'],
+['10140340132','张甜甜'],
+['10140340136','潘晨煜'],
+['10140340138','鲁梁秀'],
+['10140340141','李皓祯'],
+['10140340145','战晓瑜'],
+['10140340148','王冰倩'],
+['10140340150','李志伟']];
 void main(){
   for(var i=0;i<n;i++) {
     list[i] = 1000;
   }
-  querySelector('#output').text = '点击按钮进行学号点名';
+  querySelector('#output').text = '点击按钮进行点名';
   querySelector('#create_number').onClick.listen(generate);
 }
 void generate(MouseEvent e)
@@ -23,13 +42,13 @@ void generate(MouseEvent e)
     for (var i = 0; i < count; i++) {
         if (list[i] == num) {
           flag = false;
-          querySelector('#showme').text = "已经点过了";
+          querySelector('#showme').text = listname[num][1]+"已经点过了";
           break;
         }
       }
       if (flag == true) {
         list[count] = num;
-        querySelector('#showme').text = list[count].toString();
+        querySelector('#showme').text = listname[num][1];
         count++;
       }
     if(count>=n) querySelector('#show').text="点名结束！";
