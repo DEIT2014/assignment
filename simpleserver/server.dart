1172fe9f00d984c1df381d86e3fd3f21c5d1ef5a
panchenyu <panchenyu0808@163.com>
2016-10-18 21:36:58 +0800

    实现服务器和客户端连接
    可以在客户端现实服务器后台的文件，但是文件读取时好时不好，有时候可以读出来有时候又不可以

--- simpleserver/server.dart
+++ simpleserver/server.dart
@@ -5,36 +5,19 @@ final DATA_FILE = "data.json";//数据文件的名字
 void main() {
   HttpServer.bind(HOST, PORT).then((server) {//地址和端口进入，启动服务，开始无限循环
     server.listen((HttpRequest request) {
-      switch (request.method) {
-        case "GET"://拿到服务器中存折的数据
-          handleGet(request);
-          break;//跳出循环
-        case "POST"://建立新数据
-          handlePost(request);
-          break;
-        case "OPTIONS":
-          handleOptions(request);
-          break;
-        default: defaultHandler(request);//什么方法都没有怎么办
-      }
-    },
-        onError: printError);//出错了应该怎么办
-
-    print("Listening for GET and POST on http://$HOST:$PORT");
-  },
-      onError: printError);
+          switch(request.method){
+            case "GET":
+              handleGet(request);
+              break;
+          }
+ });
+  });
 }
-
-/**
- * Handle GET requests by reading the contents of data.json
- * and returning it to the client
- */
 void handleGet(HttpRequest req) {
   HttpResponse res = req.response;
   print("${req.method}: ${req.uri.path}");
-  addCorsHeaders(res);//浏览器安全保护策略，很重要
-
-  var file = new File(DATA_FILE);//创建一个新文件
+  addCorsHeaders(res);
+  var file = new File(DATA_FILE);
   if (file.existsSync()) {//打开此文件看它是否存在，存在即进行以下操作
     res.headers.add(HttpHeaders.CONTENT_TYPE, "application/json");//不写可能直接下载此文件了，高显速浏览器是要作为json的文本示
     file.readAsBytes().asStream().pipe(res); // automatically close output stream//pipe管道，response是用户向服务器递交的一个请求，就像一个箱子，经过处理后，再作为一个箱子送回客户端
@@ -44,58 +27,10 @@ void handleGet(HttpRequest req) {
     res.write(err);//.addString(err);
     res.close();
   }
-
 }
 
-/**
- * Handle POST requests by overwriting the contents of data.json
- * Return the same set of data back to the client.
- */
-void handlePost(HttpRequest req) {
-  HttpResponse res = req.response;
-  print("${req.method}: ${req.uri.path}");
-  addCorsHeaders(res);
-
-  req.listen((List<int> buffer) {
-    var file = new File(DATA_FILE);
-    var ioSink = file.openWrite(); // save the data to the file
-    ioSink.add(buffer);
-    ioSink.close();
-
-    // return the same results back to the client
-    res.add(buffer);
-    res.close();
-  },
-      onError: printError);
-}
-
-/**
- * Add Cross-site headers to enable accessing this server from pages
- * not served by this server
- *
- * See: http://www.html5rocks.com/en/tutorials/cors/
- * and http://enable-cors.org/server.html
- */
 void addCorsHeaders(HttpResponse res) {//涉及安全，基本不变，直接用就好
   res.headers.add("Access-Control-Allow-Origin", "*");
   res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 }
-
-void handleOptions(HttpRequest req) {//基本不处理
-  HttpResponse res = req.response;
-  addCorsHeaders(res);
-  print("${req.method}: ${req.uri.path}");
-  res.statusCode = HttpStatus.NO_CONTENT;
-  res.close();
-}
-
-void defaultHandler(HttpRequest req) {//设呢方法都没有怎么处理
-  HttpResponse res = req.response;
-  addCorsHeaders(res);
-  res.statusCode = HttpStatus.NOT_FOUND;
-  //res.addString("Not found: ${req.method}, ${req.uri.path}");
-  res.close();
-}
-
-void printError(error) => print(error);//错误处理方法
--- web/main.dart
+++ web/main.dart
@@ -1,115 +1,58 @@
 // Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
-import 'dart:convert';
 import 'dart:html';
-import'dart:math';
-import 'packages/json_object/json_object.dart';
+import 'dart:math';
 
 var host = "127.0.0.1:8080";
-var keepname =new List<int>();//要移出来不能放在generate函数当中，不然每次都生成keepname数组，数组中永远无法保存已经抽选过的num函数
-var name=new Map();
+var keepname = new List<int>(); //要移出来不能放在generate函数当中，不然每次都生成keepname数组，数组中永远无法保存已经抽选过的num函数
+var name = new Map();
 
- void main() {
-  querySelector("#load").onClick.listen(loadData);//打横线意味着此API时老的但不建议用，可改为queryslecter|按按钮下载数据，在服务器中属于get
-  querySelector("#loadStructured").onClick.listen(loadStructuredData);//下载数据，在服务器中属于get
-  querySelector("#save").onClick.listen(saveData);//保存数据，在服务器中属于post
-  querySelector('#create_number').onClick.listen(generate); //querySelector('#output').text = '随机点名程序';
+void main() {
+  querySelector("#load").onClick.listen(loadData); //打横线意味着此API时老的但不建议用，可改为queryslecter|按按钮下载数据，在服务器中属于get
+  querySelector('#create_number').onClick.listen(generate); //querySeector('#output').text = '随机点名程序';
 }
 
 void loadData(_) {
-  print("Loading data");
-  var url = "http://$host/programming-languages";//按照rest原则所有资源对应唯一的url，data.json的唯一url就是这个，有变量以后改地址容易
-  // call the web server asynchronously
-  var request = HttpRequest.getString(url).then(onDataLoaded);//HTML包里提交请求的方法，客户端是html包，io是服务器端的包
+  var url = "http://$host/programming-languages"; //按照rest原则所有资源对应唯一的url，data.json的唯一url就是这个，有变量以后改地址容易
+  var request = HttpRequest.getString(url).then(onStructuredDataLoaded); //HTML包里提交请求的方法，客户端是html包，io是服务器端的包
 }
-onDataLoaded(responseText) {//拿数据的函数，回调函数，之前不需要设置参数，直接用就可以了
+
+void onStructuredDataLoaded(responseText) {
+  //拿数据的函数，回调函数，之前不需要设置参数，直接用就可以了
   var jsonString = responseText;
   querySelector("#json_content").text = jsonString;
+  //Language jsonObject=new LanguageImpl.fromJsonString(jsonString);
+  //print("jsonObject.language = ${jsonObject.language}");
+  //for(var count = 0;count<jsonObject.targets.length;count++){
+    //print("jsonObject.target[$count]=${jsonObject.targets[count]}");
+ // }
+  //jsonObject.website.forEach(key,value){
+    //print("jsonObject.website['$key']=$value");
+  //}
 }
 
-// Load data an interperet using json object
-void loadStructuredData(_) {//结构化数据
-  print("Loading structured data");
-  var url = "http://$host/programming-languages";
-  // call the web server asynchronously
-  var request = HttpRequest.getString(url).then(onStructuredDataLoaded);
-}
-void onStructuredDataLoaded(responseText) {//把数据从文本转化为有结构的数据
-  print(" Structured data loaded");
-  var jsonString = responseText;//关于json的处理在dart convert的包里
-  querySelector("#json_content").text = jsonString;
-
-  print(" Converting to JsonObject");
-  Language jsonObject = new LanguageImpl.fromJsonString(jsonString);
-  querySelector("#json_content").text = jsonString;
-
-  print("  jsonObject.language = ${jsonObject.language}");
-
-  for (var count = 0; count < jsonObject.targets.length; count++) {
-    print("  jsonObject.targets[$count] = ${jsonObject.targets[count]}");
-  }
-
-  jsonObject.website.forEach((key,value) {//重新打印出来看清楚的数据长什么样
-    print("  jsonObject.website['$key'] = $value");
-  });
-}
-void saveData(_) {
-  print("Saving structured data");
-
-  // Setup the request
-  var request = new HttpRequest();
-  request.onReadyStateChange.listen((_) {
-    if (request.readyState == HttpRequest.DONE &&
-        (request.status == 200 || request.status == 0)) {
-      // data saved OK.
-      print(" Data saved successfully");
-
-      // update the UI
-      var jsonString = request.responseText;
-      querySelector("#json_content").text = jsonString;
-    }
-  });
-
-  // Get some data to save
-  var jsonString = querySelector("#json_content").text;
-  Language jsonObject = new LanguageImpl.fromJsonString(jsonString);
-  jsonObject.language = jsonObject.language.toUpperCase();
-  jsonObject.targets.add("Android?");
-
-  // POST the data to the server
-  var url = "http://$host/programming-languages";
-  request.open("POST", url, async: false);
-  request.send(jsonObject);//把收到的数据处理完发出去
-}
 
 abstract class Language {//定义class是为了方便结构化地处理数据
   String language;
   List targets;
   Map website;
 }
-class LanguageImpl extends JsonObject implements Language {
-  LanguageImpl();
-  factory LanguageImpl.fromJsonString(string) {
-    return new JsonObject.fromJsonString(string, new LanguageImpl());
-  }
-
-}
 
 
-void generate(MouseEvent e){
-  var randomVar=new Random( ); //引用一个random的类
-  var num=randomVar.nextInt(10);  //从10的范围中生成一个随机数,从0开始
-  if(keepname.length>10){
-    querySelector('#showme').text='本轮点名已结束，点击按钮进入新一轮点名';
+
+void generate(MouseEvent e) {
+  var randomVar = new Random(); //引用一个random的类
+  var num = randomVar.nextInt(10); //从10的范围中生成一个随机数,从0开始
+  if (keepname.length > 10) {
+    querySelector('#showme').text = '本轮点名已结束，点击按钮进入新一轮点名';
     keepname.clear();
-  }
-  else{
-    if(keepname.indexOf(num) == 0){//此语句判断num是否在数组中
-      querySelector('#showme').text='此学生本轮已点过，请再次操作';
-    }
-    else{
+  } else {
+    if (keepname.indexOf(num) == 0) {
+      //此语句判断num是否在数组中
+      querySelector('#showme').text = '此学生本轮已点过，请再次操作';
+    } else {
       keepname.add(num);
-      querySelector('#showme').text=name[num];
+      querySelector('#showme').text = name[num];
     }
   }
 }
