e40fb41fed3c48cc8cdc27fac2421b76b2f170df
DaBingXiaoQian <1962129667@qq.com>
2016-10-20 17:23:33 +0800

    最终版本
    利用数据库读取ＪＳＯＮ数据，反馈给客户端

--- dianmingxiugai/bin/server.dart
+++ dianmingxiugai/bin/server.dart
@@ -1,87 +1,84 @@
 import 'dart:io';
-import 'dart:async';
-import 'dart:math' show Random;
-var names = new List<int>();
-var remain=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-var students=
-{
-  '0':'平安',
-  '1':'张三',
-  '2':'李四',
-  '3':'王麻',
-  '4':'赵华',
-  '5':'孙悦',
-  '6':'李丽',
-  '7':'周涛',
-  '8':'陈旭',
-  '9':'朱珠',
-  '10':'陈晨',
-  '11':'张宇',
-  '12':'李伟',
-  '13':'王宁',
-  '14':'兰丹',
-  '15':'李斌',
-  '16':'魏毅',
-  '17':'韦俊',
-  '18':'孙华',
-  '19':'安安',
-  '20':'于雷'
-};
-Future main() async {
-  HttpServer requestServer =
-  await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 4041);
-  await for (var request in requestServer) {
-    handleRequest(request);
-  }
-}
-void handleRequest(HttpRequest request) {
-  try {
-    if (request.method == 'GET') {
-      handleGet(request);
-    } else {
-      request.response
-        ..statusCode = HttpStatus.METHOD_NOT_ALLOWED
-        ..write('Unsupported request: ${request.method}.')
-        ..close();
-    }
-  } catch (e) {
-    print('Exception in handleRequest: $e');
-  }
-  print('Request handled.');
-}
-void handleGet(HttpRequest request) {
-  int myNumber = new Random().nextInt(20);
-  remain.remove(myNumber);
-  request.response.statusCode = HttpStatus.OK;
-  print("The Number is: $myNumber");
-  print("The students who are already signed in:");
-  print(names.toString());
-  print("The students who are not signed in:");
-  print(remain.toString());
-  var a=names.contains(myNumber);
-  if(a)
-  {if(names.length<21){
-    request.response
-      ..writeln(students[myNumber.toString()]+"已经被点过名，请重新点名！")
-      ..write("还没有被点名的同学：")
-      ..write(remain.toString())
-      ..close();
-  }
-  else
-  {
-    request.response
-      ..write(  '您已经点过班级中所有同学的姓名，继续点击将重新开始点名！')
-      ..close();
-    names.clear();
-  }
-  }
-  else
-  {
+final HOST = "127.0.0.1"; // eg: localhost
+final PORT = 4041;
+final DATA_FILE = "data.json";
+void main() {
+  HttpServer.bind(HOST, PORT).then((server) {//返回server
+    server.listen((HttpRequest request) {//监听是否有访问
+      switch (request.method) {
+        case "GET":
+          handleGet(request);
+          break;
+        case "POST":
+          handlePost(request);
+          break;
+        case "OPTIONS":
+          handleOptions(request);
+          break;
+        default: defaultHandler(request);
+      }
+    },
+        onError: printError);
 
-    request.response
-      ..write(students[myNumber.toString()])
-      ..close();
-    names.add(myNumber);
+    print("Listening for GET and POST on http://$HOST:$PORT");
+  },
+      onError: printError);
+}
+void handleGet(HttpRequest req) {
+  HttpResponse res = req.response;//回应
+  print("${req.method}: ${req.uri.path}");
+  addCorsHeaders(res);
+
+  var file = new File(DATA_FILE);
+  if (file.existsSync()) {
+    res.headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+    file.readAsBytes().asStream().pipe(res); // automatically close output stream
+  }
+  else {
+    var err = "Could not find file: $DATA_FILE";
+    res.addError(err);
+    res.close();
   }
 
 }
+void handlePost(HttpRequest req) {
+  HttpResponse res = req.response;
+  print("${req.method}: ${req.uri.path}");
+
+  addCorsHeaders(res);
+
+  req.listen((List<int> buffer) {
+    var file = new File(DATA_FILE);
+    var ioSink = file.openWrite(); // save the data to the file
+    ioSink.add(buffer);
+    ioSink.close();
+
+    // return the same results back to the client
+    res.add(buffer);
+    res.close();
+  },
+      onError: printError);
+}
+void addCorsHeaders(HttpResponse res) {
+  res.headers.add("Access-Control-Allow-Origin", "*");
+  res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
+
+void handleOptions(HttpRequest req) {
+  HttpResponse res = req.response;
+  addCorsHeaders(res);
+  print("${req.method}: ${req.uri.path}");
+  res.statusCode = HttpStatus.NO_CONTENT;
+  res.close();
+}
+
+void defaultHandler(HttpRequest req) {
+  HttpResponse res = req.response;
+  addCorsHeaders(res);
+  res.statusCode = HttpStatus.NOT_FOUND;
+  res.write("Not found: ${req.method}, ${req.uri.path}");
+  res.close();
+}
+
+void printError(error) => print(error);
