49f133baa71d316eea6db4686845cfe245746a67
DaBingXiaoQian <1962129667@qq.com>
2016-10-24 12:30:27 +0800

    重新提交点名程序
    修改了一下文件夹的路径，之前的程序结构有问题

---
+++ dianming/bin/server.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+final HOST = "127.0.0.1"; // eg: localhost
+final PORT = 4041;
+final DATA_FILE = 'D:\\assignment\\dianmingxiugai\\bin\\data.json';
+
+void main() {
+  HttpServer.bind(HOST, PORT).then((server) {//返回server
+    server.listen((HttpRequest request) {//监听是否有访问
+      switch (request.method) {
+        case "GET":
+          handleGet(request);
+          break;
+        case "POST":
+          handlePost(request);
+          break;
+        case "OPTIONS":
+          handleOptions(request);
+          break;
+        default: defaultHandler(request);
+      }
+    },
+        onError: printError);
+
+    print("Listening for GET and POST on http://$HOST:$PORT");
+  },
+      onError: printError);
+}
+void handleGet(HttpRequest req) {
+  HttpResponse res = req.response;//回应
+  print("${req.method}: ${req.uri.path}");
+  addCorsHeaders(res);
+
+  var file = new File(DATA_FILE);
+
+  if (file.existsSync()) {
+    res.headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+    file.readAsBytes().asStream().pipe(res); // automatically close output stream
+  }
+  else {
+    var err = "Could not find file: data.json";
+    res.addError(err);
+    res.close();
+  }
+
+}
+void handlePost(HttpRequest req) {
+  HttpResponse res = req.response;
+  print("${req.method}: ${req.uri.path}");
+
+  addCorsHeaders(res);
+
+  req.listen((List<int> buffer) {
+    var file = new File(DATA_FILE);
+    var ioSink = file.openWrite(); // save the data to the file
+    ioSink.add(buffer);
+    ioSink.close();
+
+    // return the same results back to the client
+    res.add(buffer);
+    res.close();
+  },
+      onError: printError);
+}
+void addCorsHeaders(HttpResponse res) {
+  res.headers.add("Access-Control-Allow-Origin", "*");
+  res.headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
+
+void handleOptions(HttpRequest req) {
+  HttpResponse res = req.response;
+  addCorsHeaders(res);
+  print("${req.method}: ${req.uri.path}");
+  res.statusCode = HttpStatus.NO_CONTENT;
+  res.close();
+}
+
+void defaultHandler(HttpRequest req) {
+  HttpResponse res = req.response;
+  addCorsHeaders(res);
+  res.statusCode = HttpStatus.NOT_FOUND;
+  res.write("Not found: ${req.method}, ${req.uri.path}");
+  res.close();
+}
+
+void printError(error) => print(error);
